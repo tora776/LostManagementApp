8de83daff697e683e984903a09dc5579e7ebc7ee
tora776 <taichi.nakaoka5757@gmail.com>
2025-09-13 14:35:55 +0900

    20250913

--- LostManagementApp/wwwroot/js/lost.js
+++ LostManagementApp/wwwroot/js/lost.js
@@ -249,6 +249,7 @@ function createTable(data) {
     }
 // 日付を yyyy/MM/dd 形式にフォーマットする関数
 function formatDate(dateString) {
+            if (!dateString) return "";
             const date = new Date(dateString);
             return `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, "0")}/${date.getDate().toString().padStart(2, "0")}`;
         }
@@ -259,13 +260,14 @@ function getLostTextValue() {
     const lostPlace = document.getElementById("lostPlace").value;
     const isFound = document.getElementById("isFound").checked;
     const lostDetailedPlace = document.getElementById("lostDetail").value;
-    const lostDateValue = document.getElementById("lostDate").value ?? null;
-    const foundDateValue = document.getElementById("foundDate").value ?? null;
-
+    const lostDateValue = formatDate(document.getElementById("lostDate").value);
+    const foundDateValue = formatDate(document.getElementById("foundDate").value);
+    const updateDateValue = formatDate(new Date().toString());
 
     // ISO8601形式に変換（空の場合はnull）
     const lostDate = lostDateValue ? new Date(lostDateValue).toISOString() : null;
     const foundDate = foundDateValue ? new Date(foundDateValue).toISOString() : null;
+    const updateDate = updateDateValue ? new Date(updateDateValue).toISOString() : null;
 
     if (!lostItem || !lostPlace || !lostDetailedPlace) {
         alert("全ての項目を入力してください。");
@@ -273,10 +275,10 @@ function getLostTextValue() {
     }
 
     // 日本時間でyyyy/MM/dd形式の文字列を生成
-    const jpDate = new Date();
-    const jpTime = jpDate.toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
-    const dateObj = new Date(jpTime);
-    const updateDate = `${dateObj.getFullYear()}/${(dateObj.getMonth() + 1).toString().padStart(2, "0")}/${dateObj.getDate().toString().padStart(2, "0")}`;
+    //const jpDate = new Date();
+    //const jpTime = jpDate.toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
+    //const dateObj = new Date(jpTime);
+    //const updateDate = `${dateObj.getFullYear()}/${(dateObj.getMonth() + 1).toString().padStart(2, "0")}/${dateObj.getDate().toString().padStart(2, "0")}`;
 
     const data = {
         LostId: parseInt(lostId),
@@ -288,7 +290,7 @@ function getLostTextValue() {
         LostPlace: lostPlace,
         LostDetailedPlace: lostDetailedPlace,
         RegistrateDate: null, 
-        UpdateDate: updateDate // yyyy/MM/dd形式
+        UpdateDate: updateDate 
     };
 
     return data;
